354d8b87da69884f81a0fa32c3873699b8e82080
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-02-05 16:46:25 +0700

    updatecode

---
+++ Assets/Js/Home.js
@@ -0,0 +1,27 @@
+var myFullpage = new fullpage("#main", {
+    sectionsColor: [
+        "#fefefe",
+        "#fefefe",
+        "#fefefe",
+        "#fefefe", 
+        "#fefefe",
+        "#fefefe",
+        "#fefefe",
+        "#fefefe",
+        "fefefe",
+        "#fefefe",
+    ],
+    anchors: ['banner', 'categories', 'products', 'discount', 'best-sells', 'top-trending', 'download-app', 'benefits', 'infomation'],
+    menu: "#header",
+    continuousVertical: true,
+    continuousHorizontal: true,
+    navigation: true,
+    navigationTooltips: ['Trang chủ', 'Danh mục', 'Sản phẩm', 'Giảm giá', 'Sản phẩm bán chạy', 'Sản phẩm xu hướng', 'Tải ứng dụng', 'Lợi ích', 'Thông tin'],
+    showActiveTooltip: true,
+    afterLoad: function (origin, destination, direction, trigger) {
+        // console.log("afterLoad: destination:" + destination.index);
+    },
+    onLeave: function (origin, destination, direction, trigger) {
+        // console.log("onLeave: destination:" + destination.index);
+    },
+});
