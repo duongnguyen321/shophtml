53c3456fcc03d5d53003ae54767747cbff35b84e
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-02-08 11:23:04 +0700

    update main section

--- Assets/Js/Home.js
+++ Assets/Js/Home.js
@@ -7,6 +7,10 @@ var myFullpage = new fullpage("#main", {
     navigationTooltips: ['Trang chủ', 'Danh mục', 'Sản phẩm', 'Khuyễn mại', 'Ưu đãi hấp dẫn', 'Sản phẩm hàng đầu', 'Lợi ích', 'Thông tin'],
     showActiveTooltip: true,
     slidesNavigation: true,
+    paddingTop: "150px",
+    fixedElements: "#header",
+    recordHistory: false,
+    offsetSections: true,
 });
 const form = document.querySelectorAll("form")
 for (let i = 0; i < form.length; i++) {
