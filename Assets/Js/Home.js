e23219966d24a9de085e360a8d945f472664076b
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-02-11 16:20:41 +0700

    section products

--- Assets/Js/Home.js
+++ Assets/Js/Home.js
@@ -11,9 +11,27 @@ var myFullpage = new fullpage("#main", {
     fixedElements: "#header",
     recordHistory: false,
 });
+
 const form = document.querySelectorAll("form")
 for (let i = 0; i < form.length; i++) {
     form[i].addEventListener("submit", function (e) {
         e.preventDefault()
+    })
+}
+const removeClassItem = (item, classname) => {
+    if (item !== null) {
+        item.classList.remove(classname);
+    }
+}
+const addClassItem = (item, classname) => {
+    if (item !== null) {
+        item.classList.add(classname);
+    }
+}
+const categories_list_item = document.querySelectorAll(".categories_list-item")
+for (let i = 0; i < categories_list_item.length; i++) {
+    categories_list_item[i].addEventListener("click", () => {
+        removeClassItem(document.querySelector('.categories_list-item.active'), "active")
+        addClassItem(categories_list_item[i], "active")
     })
 }
